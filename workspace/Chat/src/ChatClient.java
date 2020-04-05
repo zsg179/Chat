13b6af8e219a5db3bbd9239ae76c6e7d1a40fd88
zsg179 <18977330642@163.com>
2020-04-05 11:14:27 +0800

    0.2 创建文本框

--- workspace/Chat/src/ChatClient.java
+++ workspace/Chat/src/ChatClient.java
@@ -7,9 +7,14 @@ import java.awt.*;
  * @Version :1.0
  */
 public class ChatClient extends Frame {
+    TextField tfTxt = new TextField();
+    TextArea taContent = new TextArea();
     public void launchFrame() {
         this.setLocation(400,300);
         this.setSize(300,300);
+        add(tfTxt, BorderLayout.SOUTH);
+        add(taContent, BorderLayout.NORTH);
+        pack();
         this.setVisible(true);
     }
 
