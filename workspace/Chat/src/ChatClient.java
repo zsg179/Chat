7ebf293a497e0ab3210a6f8b712d2dbd15b71ee5
zsg179 <18977330642@163.com>
2020-04-05 12:53:05 +0800

    0.4 为文本框添加监听器

--- workspace/Chat/src/ChatClient.java
+++ workspace/Chat/src/ChatClient.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -11,9 +13,10 @@ import java.awt.event.WindowEvent;
 public class ChatClient extends Frame {
     TextField tfTxt = new TextField();
     TextArea taContent = new TextArea();
+
     public void launchFrame() {
-        this.setLocation(400,300);
-        this.setSize(300,300);
+        this.setLocation(400, 300);
+        this.setSize(300, 300);
         add(tfTxt, BorderLayout.SOUTH);
         add(taContent, BorderLayout.NORTH);
         pack();
@@ -23,11 +26,21 @@ public class ChatClient extends Frame {
                 System.exit(0);
             }
         });
+        tfTxt.addActionListener(new TFListener());
         this.setVisible(true);
     }
 
     public static void main(String[] args) {
         new ChatClient().launchFrame();
+    }
+
+    private class TFListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String s = tfTxt.getText().trim();
+            taContent.setText(s);
+            tfTxt.setText("");
+        }
     }
 }
 
